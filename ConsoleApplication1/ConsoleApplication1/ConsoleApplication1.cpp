6c8ec83a16e27fbe5af198685695cc198611b1e7
IAmAllOfMe <Benanorman@live.com>
2016-10-24 14:35:23 +0100

    Sup duddddeeessss

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello you poo" << endl;
+	int pause;
+	cin >> pause;
+    return 0;
+}
+
